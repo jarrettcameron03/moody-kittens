be5b91bba602750bc229cf1ab64df634496ddaad
Jarrett Cameron <native@hotmail.com>
2024-03-11 19:51:20 -0600

    Finished project (pretend I made commits throughout LOL)

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 let kittens = []
 /**
  * Called when submitting the new Kitten Form
@@ -7,13 +9,35 @@ let kittens = []
  * Then reset the form
  */
 function addKitten(event) {
+  event.preventDefault();
+
+  let nameAvailable = true;
+  kittens.forEach((kitten) => {
+    nameAvailable = !(kitten.name == document.getElementById('kittenName').value);
+  });
+
+  if (!nameAvailable) {
+    alert("Kitten name is already taken.");
+    return;
+  }
+
+  kittens.push({
+    "id": generateId(),
+    "name": document.getElementById('kittenName').value,
+    "mood": "Tolerant",
+    "affection": 3
+  });
+  document.getElementById('kittenName').value = "";
+  saveKittens();
 }
 
 /**
  * Converts the kittens array to a JSON string then
- * Saves the string to localstorage at the key kittens 
+ * Saves the string to localstorage at the key kittens
  */
 function saveKittens() {
+  window.localStorage.setItem("kittens", JSON.stringify(kittens));
+  drawKittens();
 }
 
 /**
@@ -22,33 +46,71 @@ function saveKittens() {
  * the kittens array to the retrieved array
  */
 function loadKittens() {
+  kittens = JSON.parse(window.localStorage.getItem("kittens"));
+  drawKittens();
 }
 
 /**
  * Draw all of the kittens to the kittens element
  */
 function drawKittens() {
+  document.getElementById('kittens').innerHTML = "";
+  if (kittens.length > 0) {
+    kittens.forEach((kitten) => {
+      if (document.getElementById(kitten.id) == null) {
+        let kittenContainer = document.createElement("div");
+        kittenContainer.id = kitten.id;
+        kittenContainer.className = `kitten ${kitten.mood.toLowerCase()}`;
+        kittenContainer.innerHTML = `<img src='https://pngfre.com/wp-content/uploads/transparent-cat-by-pngfre-74-243x300.png'><h3>Name: ${kitten.name}</h3><p class='stats'>Mood: ${kitten.mood}<br>Affection: ${kitten.affection}</p><button onclick="pet('${kitten.id}')">Pet</button><button onclick="catnip('${kitten.id}')">Give Catnip</button><button onclick="deleteCat('${kitten.id}')" class="red" style="pointer-events: auto !important;">Delete Cat</button>`;
+        document.getElementById('kittens').append(kittenContainer);
+      }
+    });
+  }
 }
 
+/**
+ * Delete kitten by ID
+ * @param {string} id
+ */
+
+function deleteCat(id) {
+  let kitten = findKittenById(id);
+  kittens.splice(kitten, 1);
+  saveKittens();
+}
 
 /**
  * Find the kitten in the array by its id
- * @param {string} id 
+ * @param {string} id
  * @return {Kitten}
  */
 function findKittenById(id) {
+  let foundKitten = null;
+  kittens.forEach((kitten) => {
+    if (kitten.id == id) {
+      foundKitten = kitten;
+    }
+  });
+  return foundKitten;
 }
 
 
 /**
  * Find the kitten in the array of kittens
  * Generate a random Number
- * if the number is greater than .5 
+ * if the number is greater than .5
  * increase the kittens affection
  * otherwise decrease the affection
- * @param {string} id 
+ * @param {string} id
  */
 function pet(id) {
+  let kitten = findKittenById(id);
+  if (Math.random() >= 0.5) {
+    kitten.affection = Math.min(kitten.affection + 1, 5);
+  } else {
+    kitten.affection--;
+  }
+  setKittenMood(kitten);
 }
 
 /**
@@ -58,29 +120,51 @@ function pet(id) {
  * @param {string} id
  */
 function catnip(id) {
+  let kitten = findKittenById(id);
+  kitten.mood = "Tolerant";
+  kitten.affection = 5;
+  setKittenMood(kitten);
 }
 
 /**
  * Sets the kittens mood based on its affection
- * @param {Kitten} kitten 
+ * @param {Kitten} kitten
  */
 function setKittenMood(kitten) {
+  switch (kitten.affection) {
+    case 0:
+      kitten.mood = "Gone";
+      break;
+    case 2, 3, 4:
+      kitten.mood = "Tolerant";
+      break;
+    case 1:
+      kitten.mood = "Angry";
+      break;
+    case 5:
+      kitten.mood = "Happy";
+      break;
+  }
+  saveKittens();
 }
 
 /**
  * Removes all of the kittens from the array
  * remember to save this change
  */
-function clearKittens(){
+function clearKittens() {
+  kittens = [];
+  saveKittens();
 }
 
 /**
- * Removes the welcome content and should probably draw the 
+ * Removes the welcome content and should probably draw the
  * list of kittens to the page. Good Luck
  */
 function getStarted() {
   document.getElementById("welcome").remove();
-  console.log('Good Luck, Take it away')
+  console.log('Good Luck, Take it away');
+  loadKittens();
 }
 
 
@@ -100,5 +184,3 @@ function getStarted() {
 function generateId() {
   return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
 }
-
-loadKittens();
